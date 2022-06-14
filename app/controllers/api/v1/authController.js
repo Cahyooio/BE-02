631153132be7680dc6473a1dffc881f1148950f2
Farhan Septiadi <55498270+suzuya26@users.noreply.github.com>
2022-06-14 21:20:12 +0700

    jwt fix

--- app/controllers/api/v1/authController.js
+++ app/controllers/api/v1/authController.js
@@ -2,7 +2,7 @@
  * @file contains authentication request handler and its business logic
  * @author Fikri Rahmat Nurhidayat
  */
-
+ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { User } = require("../../../models");
 const SALT = 10;
@@ -21,7 +21,7 @@ function encryptPassword(password) {
 }
 
 
-function checkPassword(password) {
+function checkPassword(encryptedPassword, password) {
   return new Promise((resolve, reject) => {
     bcrypt.compare(password, encryptedPassword, (err, isPasswordCorrect) => {
       if (!!err) {
@@ -33,6 +33,10 @@ function checkPassword(password) {
     });
   });
 }
+function createToken(payload) {
+  return jwt.sign(payload, process.env.JWT_SIGNATURE_KEY || "Rahasia");
+}
+
 
 module.exports = {
   async register(req, res) {
@@ -63,19 +67,25 @@ module.exports = {
     }
 
     const isPasswordCorrect = await checkPassword(
-      user.encryptedPassword,
+      user.password,
       password
     );
+    const token = createToken({
+      id: user.id,
+      name : user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    })
 
     if (!isPasswordCorrect) {
       res.status(401).json({ message: "Password salah!" });
       return;
     }
-
     res.status(201).json({
       id: user.id,
       email: user.email,
-      token: "iniToken", // Kita bakal ngomongin ini lagi nanti.
+      token: token, // Kita bakal ngomongin ini lagi nanti.
       createdAt: user.createdAt,
       updatedAt: user.updatedAt,
     });
--- db/migrations/20220417134102-create-user.js
+++ db/migrations/20220417134102-create-user.js
@@ -2,7 +2,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Users", {
-      iduser: {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
